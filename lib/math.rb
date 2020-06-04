465c0ea0826d4413ad45dd005dbdefb0a7358651
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-04 14:50:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  add 
 end
 
 def subtraction(num1, num2)
-
+  subtract = num1 - num2
+  subtract
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2 
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  multiply
 end
 
 def modulo(num1, num2)
-
+  modulo_num = num1 % num2
+  modulo_num 
 end
 
 def square_root(num)
-
+  
 end
